0affe1e62fe3c28e1eb576b1ee6a50a915463146
anuj19aneupane <anujaneupane20@gmail.com>
2024-01-04 19:00:58 +0545

    enum 1

---
+++ 14_structures,unions,enums/5_enum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+
+    enum Meal
+    {
+        breakfast,
+        lunch,
+        dinner
+    };
+    std::cout << breakfast << std::endl;
+    std::cout << lunch << std::endl;
+    std::cout << dinner << std::endl;
+
+    return 0;
+}
