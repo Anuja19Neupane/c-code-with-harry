21e640b0acb6d7b1f5ef716d5142ab3c4128a4d3
anuj19aneupane <anujaneupane20@gmail.com>
2024-01-04 19:00:16 +0545

    typedef structure

---
+++ 14_structures,unions,enums/2_struct_using_typedef.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+typedef struct employee
+{
+    int employee_id;
+    char fav_char;
+    float salary;
+}emp;
+
+int main()
+{
+
+    emp anuja;
+    anuja.employee_id = 01;
+    anuja.fav_char = 'g';
+    anuja.salary = 1278.34;
+
+    emp tilasma;
+    tilasma.employee_id = 02;
+    tilasma.fav_char = 'j';
+    tilasma.salary = 12756.34;
+
+    emp binita;
+    binita.employee_id = 03;
+    binita.fav_char = 'c';
+    binita.salary = 12546.34;
+
+    std::cout << "ANUJA'S INFORMATION" << std::endl;
+    std::cout<<"The value is "<<anuja.employee_id<<std::endl;
+    std::cout<<"The value is "<<anuja.fav_char<<std::endl;
+    std::cout<<"The value is "<<anuja.salary<<std::endl;
+
+    std::cout << "TILASMA'S INFORMATION" << std::endl;
+    std::cout<<"The value is "<<tilasma.employee_id<<std::endl;
+    std::cout<<"The value is "<<tilasma.fav_char<<std::endl;
+    std::cout<<"The value is "<<tilasma.salary<<std::endl;
+
+    std::cout << "BINITA'S INFORMATION" << std::endl;
+    std::cout<<"The value is "<<binita.employee_id<<std::endl;
+    std::cout<<"The value is "<<binita.fav_char<<std::endl;
+    std::cout<<"The value is "<<binita.salary<<std::endl;
+
+    return 0;
+}
